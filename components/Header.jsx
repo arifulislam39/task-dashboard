8ce6ef431f78adb81d41abfdd3f964df8aaf6341
ariful-islam <arifulislam204909@gmail.com>
2023-08-05 21:23:46 +0600

    count container added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,37 +1,38 @@
-import React from 'react';
+import React from "react";
+import { FiSettings } from "react-icons/fi";
+import { BiPlus } from "react-icons/bi";
 
 const Header = () => {
-    return (
-        <div className='bg-red-300 mt-0'>
-           <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  return (
+    <div className="bg-red-300 mt-0">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+
+          <h3 className=" normal-case font-bold text-xl ">Add New Post</h3>
+
+          <div className="flex items-center px-4">
+            <BiPlus />
+            <h3 className="p-2">Add Content</h3>
+          </div>
+
+          <div className="flex items-center">
+            <FiSettings />
+            <h3 className="p-2">Settings</h3>
+          </div>
+
         </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search Content"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- components/TotalCount.jsx
+++ components/TotalCount.jsx
@@ -1,17 +1,82 @@
-import React from 'react';
+import React from "react";
+import { BiShoppingBag } from "react-icons/bi";
+import { BsShop, BsPeopleFill } from "react-icons/bs";
 
 const TotalCount = () => {
-    return (
-        <div className='bg-yellow-300'>
-            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-6 '>
-                <div className='bg-red-200 w-40 h-24'><h1>1</h1></div>
-                <div className='bg-slate-300 w-40 h-24'><h1>2</h1></div>
-                <div className='bg-lime-500 w-40 h-24'><h1>3</h1></div>
-                <div className='bg-lime-500 w-40 h-24'><h1>3</h1></div>
+  return (
+    <div className="">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+        {/* container one */}
+        <div className="bg-white rounded-lg w-64 h-32 text-center ">
+          <div className="flex p-6 items-center">
+            <div>
+              <BiShoppingBag className="text-red-500 w-24 h-14">
+                {" "}
+              </BiShoppingBag>
             </div>
-           
+            <div>
+              <p>Total Sales</p>
+              <p className="text-2xl font-bold">$2,456</p>
+            </div>
+          </div>
         </div>
-    );
+
+        {/* container two */}
+
+
+        <div className="bg-white rounded-lg w-64 h-32 text-center ">
+          <div className="flex p-6 items-center">
+            <div>
+              <BsShop className="text-indigo-800 w-24 h-14">
+                {" "}
+              </BsShop>
+            </div>
+            <div>
+              <p className="">Total Expenses</p>
+              <p className="text-2xl font-bold">$2,456</p>
+            </div>
+          </div>
+        </div>
+
+
+        {/* container three */}
+
+
+        <div className="bg-white rounded-lg w-64 h-32 text-center ">
+          <div className="flex p-6 items-center">
+            <div>
+              <BsPeopleFill className="text-green-500 w-24 h-14">
+                {" "}
+              </BsPeopleFill>
+            </div>
+            <div>
+              <p>Total Visitors</p>
+              <p className="text-2xl font-bold">$2,456</p>
+            </div>
+          </div>
+        </div>
+
+        {/* container four */}
+
+
+        <div className="bg-white rounded-lg w-64 h-32 text-center ">
+          <div className="flex p-6 items-center">
+            <div>
+              <BiShoppingBag className="text-indigo-800 w-24 h-14">
+                {" "}
+              </BiShoppingBag>
+            </div>
+            <div>
+              <p>Total Orders</p>
+              <p className="text-2xl font-bold">$2,456</p>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
 };
 
 export default TotalCount;
